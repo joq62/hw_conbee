6d550c0b175d577391d8a1328b18e315a368b1e4
joq62 <jakim.leche@gmail.com>
2022-12-13 22:45:34 +0100

    ud

--- tests/hw_conbee_tests.erl
+++ tests/hw_conbee_tests.erl
@@ -15,7 +15,7 @@
 %% --------------------------------------------------------------------
 %% Include files
 %% --------------------------------------------------------------------
-
+-define(hw_conbee_node ,do_test@c201).
 
 %% --------------------------------------------------------------------
 %% Function: available_hosts()
@@ -26,8 +26,9 @@ start()->
     io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME}]),
 
     ok=setup(),
-  %  ok=test_1(),
-   % ok=test_2(),
+    ok=lights_test(),
+    ok=sensors_test(),
+  
     
   
   
@@ -41,19 +42,60 @@ start()->
 %% Description: Based on hosts.config file checks which hosts are avaible
 %% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
 %% --------------------------------------------------------------------
-test_2()->
+switches_test()->
     io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME}]),
+    All=rpc:call(?hw_conbee_node,hw_conbee,get_all_device_info,["switch"],2*5000),
+    io:format("All ~p~n",[{All, ?MODULE,?FUNCTION_NAME}]),
 
-  
+    AllNames=[Name||{Name,_Id,_DeviceType,_Map}<-All],
+    io:format("AllNames ~p~n",[{AllNames, ?MODULE,?FUNCTION_NAME}]),
+
+    Reg1=[{rpc:call(?hw_conbee_node,hw_conbee,get,[Name,"lights"],2*5000),Name}||Name<-AllNames],
+    io:format("Reg1 ~p~n",[{Reg1, ?MODULE,?FUNCTION_NAME}]),
+
+    Registred=[Name||{{ok,_},Name}<-Reg1],
+    io:format("Registred ~p~n",[{Registred, ?MODULE,?FUNCTION_NAME}]),
+
     ok.
 %% --------------------------------------------------------------------
 %% Function: available_hosts()
 %% Description: Based on hosts.config file checks which hosts are avaible
 %% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
 %% --------------------------------------------------------------------
-test_1()->
+sensors_test()->
     io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME}]),
+    All=rpc:call(?hw_conbee_node,hw_conbee,get_all_device_info,["sensors"],2*5000),
+    io:format("All ~p~n",[{All, ?MODULE,?FUNCTION_NAME}]),
 
+    AllNames=[Name||{Name,_Id,_DeviceType,_Map}<-All],
+    io:format("AllNames ~p~n",[{AllNames, ?MODULE,?FUNCTION_NAME}]),
+
+    Reg1=[{rpc:call(?hw_conbee_node,hw_conbee,get,[Name,"lights"],2*5000),Name}||Name<-AllNames],
+    io:format("Reg1 ~p~n",[{Reg1, ?MODULE,?FUNCTION_NAME}]),
+
+    Registred=[Name||{{ok,_},Name}<-Reg1],
+    io:format("Registred ~p~n",[{Registred, ?MODULE,?FUNCTION_NAME}]),
+
+    ok.
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+lights_test()->
+    io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME}]),
+    AllLights=rpc:call(?hw_conbee_node,hw_conbee,get_all_device_info,["lights"],2*5000),
+    io:format("AllLights ~p~n",[{AllLights, ?MODULE,?FUNCTION_NAME}]),
+
+    AllNames=[Name||{Name,_Id,_DeviceType,_Map}<-AllLights],
+    io:format("AllNames ~p~n",[{AllNames, ?MODULE,?FUNCTION_NAME}]),
+
+    LightsRegistred1=[{rpc:call(?hw_conbee_node,hw_conbee,get,[Name,"lights"],2*5000),Name}||Name<-AllNames],
+    io:format("LightsRegistred1 ~p~n",[{LightsRegistred1, ?MODULE,?FUNCTION_NAME}]),
+
+    LightsRegistred=[Name||{{ok,_},Name}<-LightsRegistred1],
+    io:format("LightsRegistred ~p~n",[{LightsRegistred, ?MODULE,?FUNCTION_NAME}]),
 
     ok.
 
@@ -73,15 +115,6 @@ test_1()->
 setup()->
     io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME}]),
 
-    ConbeeAddr="172.17.0.2",
-    ConbeePort=80,
-    Crypto="4B3F1542C5",
-
-    {ok,_}=hw_conbee_server:start(ConbeeAddr,ConbeePort,Crypto),
-    {ok,_}=common:start(),
-    pong=hw_conbee:ping(),
-    pong=common:ping(),
-    
-    
+    pong=net_adm:ping(?hw_conbee_node),   
 
     ok.
