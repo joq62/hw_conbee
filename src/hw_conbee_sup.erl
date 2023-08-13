e12f2aa170feb0ae2c245c300c5c63fdea3000a0
joq62 <jakim.leche@gmail.com>
2023-08-13 11:03:54 +0200

    ud

--- src/hw_conbee_sup.erl
+++ src/hw_conbee_sup.erl
@@ -31,7 +31,7 @@ init([]) ->
                  period => 1},
     ChildSpecs = [
 		  #{id=>tradfri_bulb_E27_cws_806lm,
-		    start=>{tradfri_bulb_E27_cws_806lm,start_link,[]}}
+		    start=>{tradfri_bulb_E27_cws_806lm,start_link,[]}},
 		  #{id=>hw_conbee,
 		    start=>{hw_conbee,start_link,[]}}		 	
 		 ],
